d41d796eabe7afbcfa3ca282a8d6ff3c9e768a45
mmgoto <ma_makoto_goto@hotmail.com>
2023-10-14 21:01:07 -0300

    first commit

---
+++ src/main/java/br/com/mmgoto/todolist/task/TaskModel.java
@@ -0,0 +1,50 @@
+package br.com.mmgoto.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    private UUID idUser;
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+    public void setTitle(String title) throws Exception{
+        if(title.length()  > 50){
+            throw new Exception("A titulo não pode conter mais de 50 caracteres.");
+        }
+        this.title = title;
+
+    }
+
+    public void setDescription(String description) throws Exception{
+        if(description.length()  > 255){
+            throw new Exception("A descrição não pode conter mais de 255 caracteres.");
+        }
+        this.description = description;
+    }
+
+    
+}
